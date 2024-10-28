efd4af14d8dd29b904320ca21145ba10077f9906
Ankita Jagtap <ankitajagtap2004@gmail.com>
2024-10-28 17:30:09 +0530

    Add files via upload
    Assignment1

---
+++ Profile.js
@@ -0,0 +1,8 @@
+// Add event listener to nav links
+document.querySelectorAll('nav a').forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = document.querySelector(e.target.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
